b21eec951c67245c9cb216960278ca7c52e5e0cf
johanneshassenstein <johannes.hassenstein@protonmail.com>
2020-03-19 14:29:44 +0100

    Removed test

--- static/credit.js
+++ static/credit.js
@@ -1,5 +1,5 @@
 window.addEventListener('load', function(){
   var credit = document.getElementById('credit');
   var year = new Date().getFullYear();
-  credit.outerHTML = '<a id="credit" href="https://branko3000.de" target="_blank" style="position: fixed; bottom: 1em; right: 1em; font-size: 1em; line-height: 1em; color: inherit; background: inherit; text-decoration: none;">(C)' + year + ' Branko3000 LOL</a>'
+  credit.outerHTML = '<a id="credit" href="https://branko3000.de" target="_blank" style="position: fixed; bottom: 1em; right: 1em; font-size: 1em; line-height: 1em; color: inherit; background: inherit; text-decoration: none;">(C)' + year + ' Branko3000</a>'
 });
