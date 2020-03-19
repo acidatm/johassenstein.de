aca7ee456ebb5494186761e0947124dfb9f421fb
johanneshassenstein <johannes.hassenstein@protonmail.com>
2020-03-19 14:35:07 +0100

    Added log

--- static/credit.js
+++ static/credit.js
@@ -1,5 +1,6 @@
 window.addEventListener('load', function(){
   var credit = document.getElementById('credit');
   var year = new Date().getFullYear();
-  credit.outerHTML = '<a id="credit" href="https://branko3000.de" target="_blank" style="position: fixed; bottom: 1em; right: 1em; font-size: 1em; line-height: 1em; color: inherit; background: inherit; text-decoration: none;">(C)' + year + ' Branko3000</a>'
+  credit.outerHTML = '<a id="credit" href="https://branko3000.de" target="_blank" style="position: fixed; bottom: 0.2em; left: 0.2em; font-size: 0.8em; line-height: 0.8em; color: inherit; background: inherit; text-decoration: none;">(C)' + year + ' Branko3000</a>'
+  console.log("This website is created by Branko3000. Check him out: https://branko3000.de")
 });
